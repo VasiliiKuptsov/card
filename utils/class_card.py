5f9cccabaecf60080a48e04feca36304008ef90c
VyskeyK <robintyan96@gmail.com>
2023-07-16 00:50:15 +0300

    card

---
+++ utils/class_card.py
@@ -0,0 +1,9 @@
+
+class Cards:
+    def __init__(self,date:str , :int, correct_answer):
+       self.question_text = question_text
+       self.complexity_issue = complexity_issue
+       self.correct_answer = correct_answer
+       self.question_asked = False
+       self.user_response = None
+       self.points_question = 0
